b76b596feac690181288d7684d6e0bb213b87121
SushrithKbtech <155775482+SushrithKbtech@users.noreply.github.com>
2025-01-10 14:15:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; // Ensure this file exists
-import App from "./App"; // Ensure this file exists
+import "./index.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
